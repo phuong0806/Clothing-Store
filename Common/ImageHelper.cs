026e212bbc022d6470d571635f56dde0e83700ef
nguyenthanhthuy2312 <39694247+nguyenthanhthuy2312@users.noreply.github.com>
2018-06-08 22:42:33 +0700

    .
    .

--- Common/ImageHelper.cs
+++ Common/ImageHelper.cs
@@ -2,11 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
-using System.Web.Script.Serialization;
 
 namespace Common
 {
